b8652eb9bb4698ded4d6d7d391a4a69c8454a785
justweb <justweb.rs@gmail.com>
2025-08-21 01:13:26 +0200

    asdaddsa

--- src/components/sections/home/FAQSection.vue
+++ src/components/sections/home/FAQSection.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
   import { onMounted, ref, nextTick  } from "vue";
   import LabelComponent from '@/components/sections/home/partials/LabelComponent.vue';
+import animateCharacters from "@/helpers/AnimatedHeadings";
 
-  import animateCharacters from "../../../helpers/AnimatedHeadings.ts";
 
   const activeFAQ = ref<number | null>(null);
   const headingRef = ref<HTMLHeadingElement | null>(null);
